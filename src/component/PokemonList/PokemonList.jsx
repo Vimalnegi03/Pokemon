40028381f2eae78e0243f310736c2cc3f28978e2
Vimal <vm.21cs48@thdcihet.ac.in>
2024-07-29 12:42:06 +0530

    Added pagination

--- src/component/PokemonList/PokemonList.jsx
+++ src/component/PokemonList/PokemonList.jsx
@@ -5,11 +5,16 @@ import Pokemon from '../Pokemon/Pokemon';
 export default function PokemonList() {
     const [pokemonList, setPokemonList] = useState([]);
     const [loading, setLoading] = useState(true);
-
+    const[pokedexUrl,setPokedexurl]=useState("https://pokeapi.co/api/v2/pokemon")
+    const [prevUrl,setPrevUrl]=useState("");
+    const[nextUrl,setNextUrl]=useState("");
     async function downloadPokemons()
     {
-        const response=await axios.get("https://pokeapi.co/api/v2/pokemon")
+        setLoading(true)
+        const response=await axios.get(pokedexUrl)
         console.log(response.data);//response.data contains the data that we need
+        setNextUrl(response.data.next)
+        setPrevUrl(response.data.previous)
         const PokemonResults=response.data.results;
         console.log(response.data.results);
         //now let us fetch the main data
@@ -32,15 +37,15 @@ export default function PokemonList() {
     }
     useEffect(()=>{
         downloadPokemons()
-    },[])
+    },[pokedexUrl])
   return (
     <div className='pokemon-list-wrapper'>
       <div className='pokemon-wrapper'> {(loading)?"Loading....":pokemonList.map((poke)=>(
         <Pokemon name={poke.name} image={poke.image} type={poke.type} key={poke.id}/>
       ))}</div>
      <div className='controls'>
-        <button>Prev</button>
-        <button>Next</button>
+        <button disabled={prevUrl==undefined} onClick={()=>setPokedexurl(prevUrl)}>Prev</button>
+        <button disabled={nextUrl==undefined} onClick={()=>setPokedexurl(nextUrl)}>Next</button>
      </div>
     </div>
   )
